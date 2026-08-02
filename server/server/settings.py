3c8e571231603d32962b02de5ca40a16bca89d9e
biswapulan <basantbiswa04@gmail.com>
2026-08-02 18:55:12 +0530

    Add STATIC_ROOT and Whitenoise storage for production static files

--- server/server/settings.py
+++ server/server/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MongoDB is used directly by the accounts app for user auth data.
 MONGO_URI = config('MONGO_URI')
